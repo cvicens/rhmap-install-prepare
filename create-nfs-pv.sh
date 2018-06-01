c1f751105bc49c59f954e4d37254397157584172
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2018-06-01 16:13:59 +0200

    check-pvs

---
+++ create-nfs-pv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ "$#" -ne 4 ]; then
+    echo "$0 <NFS_SERVER> <NFS_DIR> <PV_SIZE> <PV_QUANTITY>"
+fi
+
+NFS_SERVER=$1
+NFS_DIR=$2
+PV_SIZE=$3
+PV_QUANTITY=$4
+
+echo "${NFS_SERVER} ${PV_SIZE} ${PV_QUANTITY}"
+
+for (( i=1; i<=${PV_QUANTITY}; i++ ))
+do
+PV_NAME="rhm_pv_${PV_SIZE}_$i"
+cat << EOF > ./dyn1-pv-create.yaml
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+ name: ${PV_NAME}
+spec:
+ capacity:
+   storage: ${PV_SIZE}Gi
+ accessModes:
+   - ReadWriteOnce
+ persistentVolumeReclaimPolicy: Recycle
+ nfs:
+   path: ${NFS_DIR}/${PV_NAME}
+   server: $NFS_SERVER
+   readOnly: false
+EOF
+oc create -f ./dyn1-pv-create.yaml
+done
