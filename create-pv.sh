298025433ab50fdb83f2cc531c412cc54235a683
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2018-06-28 00:21:42 +0200

    fixin create_pvs

--- create-pv.sh
+++ create-pv.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 if [ "$#" -ne 3 ]; then
-    echo "$0 <NFS_SERVER> <PV_NAME> <PV_SIZE>"
+    echo "$0 <LOGIN_URL> <OC_TOKEN> <NFS_SERVER> <PV_NAME> <PV_SIZE>"
     exit 0
 fi
 
-NFS_SERVER=$1
-PV_NAME=$2
-PV_SIZE=$3
+LOGIN_URL=$1
+OC_TOKEN=$2
+NFS_SERVER=$3
+PV_NAME=$4
+PV_SIZE=$5
 
 cat << EOF > ./pv-create.yaml
 apiVersion: v1
@@ -25,6 +27,7 @@ spec:
    server: $NFS_SERVER
    readOnly: false
 EOF
+oc login ${LOGIN_URL} --token=${OC_TOKEN}
 oc create -f ./pv-create.yaml
 
 done
